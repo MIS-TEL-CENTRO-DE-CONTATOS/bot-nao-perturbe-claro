f87d10560bc7187687a03db0814fc3300d7a63a9
blackdot404 <blackstv11@gmail.com>
2023-05-31 15:25:57 -0300

    ✅ Download, Extrair e Agendamento OK

---
+++ getFiles.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+
+import pysftp
+import patoolib
+import os
+import locale
+
+
+locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
+
+
+class Rpa_Ftp:
+    def __init__(self, ftpHost: str, ftpUser: str, ftpPass: str):
+
+        if ftpHost is None:
+            raise ValueError("O parâmetro 'host' não pode ser None.")
+        else:
+            cnopts = pysftp.CnOpts()
+            cnopts.hostkeys = None  # Desativa a verificação de chave de host
+            self.ftp = pysftp.Connection(
+                ftpHost, username=ftpUser, password=ftpPass, cnopts=cnopts)
+
+            self.ftp.chdir('/app/sftp/bloqueio')
+
+            # self.filename = 'NAO_PERTURBE_20230530.zip'
+            self.filename = f'NAO_PERTURBE_{datetime.today().year}{datetime.today().month}{datetime.today().day}.zip'
+
+    def getFile(self):
+        mes_str = datetime.today().strftime('%B').upper()
+        mes_num = datetime.today().month
+        dia_num = datetime.today().day
+        ano_num = datetime.today().year
+
+        # caminho que irá salvar o arquivo zipado
+        path_blacklist = rf'\\TELFSBAR01\Planejamento$\{ano_num}\43 - MIS\06 - GC_CLARO\{mes_num}-{mes_str}\01-NAOPERTURBE\{dia_num}'
+
+        # verifica se o caminho existe
+        try:
+            # tenta criar os diretórios
+            os.makedirs(path_blacklist)
+        except OSError:
+            # em caso de erro, criar somente a última pasta
+            if not os.path.isdir(path_blacklist):
+                os.mkdir(path_blacklist)
+            else:
+                pass
+
+        path_blacklist_p = rf'{path_blacklist}\{self.filename}'
+
+        print(self.ftp.listdir())
+
+        # executa o processo de captura do arquivo
+        try:
+            self.ftp.get(self.filename, path_blacklist_p)
+        except Exception as e:
+            print(e)
+
+        self.ftp.close()
+
+        patoolib.extract_archive(path_blacklist_p, outdir=path_blacklist)
---
+++ main.py
@@ -0,0 +1,46 @@
+from getFiles import Rpa_Ftp
+
+import os
+import locale
+import schedule
+import time
+
+ftpHost = '172.25.64.11'
+ftpUser = 'net_telematica'
+ftpPass = 'rDuFGam0S@'
+
+# Define a localização do programa como português do Brasil
+locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
+
+
+def limpar_console():
+    if os.name == 'posix':  # Para Linux/Mac
+        _ = os.system('clear')
+    else:  # Para Windows
+        _ = os.system('cls')
+
+
+def execProcesso():
+    impObj = Rpa_Ftp(ftpHost, ftpUser, ftpPass)
+    impObj.getFile()
+
+
+# Agendar execução
+schedule.every().day.at('15:21').do(execProcesso)
+
+
+# Produção
+try:
+    while True:
+        tempo_restante = schedule.idle_seconds()
+        if tempo_restante is not None:
+            horas = int(tempo_restante // 3600)
+            minutos = int((tempo_restante % 3600) // 60)
+            segundos = int(tempo_restante % 60)
+            print('Robô Claro, importação de NÃO PERTURBE:\n',
+                  f'Próxima execução em {horas} horas, {minutos} minutos e {segundos} segundos.')
+        time.sleep(1)
+        limpar_console()
+        schedule.run_pending()
+except KeyboardInterrupt:
+    pass
