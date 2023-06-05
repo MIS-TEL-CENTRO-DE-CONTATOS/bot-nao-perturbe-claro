035e185296674ac86e91ee2682891f6464e1dd38
blackdot404 <blackstv11@gmail.com>
2023-06-05 12:58:20 -0300

    👷 Add freeze build

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os", "locale", "time"], "includes": [
+    "patoolib", "paramiko", "pysftp", "schedule"]}
+
+# GUI applications require a different base on Windows (the default is for
+# a console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="Bot Nao Perturbe",
+    version="0.1",
+    description="Bot Claro - MIS TEL CONTACT CENTER",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)]
+)
